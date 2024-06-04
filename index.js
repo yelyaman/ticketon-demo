4cc0e980fda549a1613aa1309e692c1327e82716
Yelaman Uisunbayev <yelaman.ui@gmail.com>
2024-06-04 17:59:28 +0500

    formatted code

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import middlewares from './src/middlewares/index.js';
-import authorization from './src/resources/auth/router.js';
-import files from './src/resources/files/router.js';
-import db from './src/db/index.js';
-import { RedisCacheClient } from './src/utils/cache.js';
-dotenv.config();
+import dotenv from 'dotenv'
+import express from 'express'
+import middlewares from './src/middlewares/index.js'
+import authorization from './src/resources/auth/router.js'
+import files from './src/resources/files/router.js'
+import db from './src/db/index.js'
+import { RedisCacheClient } from './src/utils/cache.js'
+import helmet from 'helmet'
+dotenv.config()
 
 try {
-  const app = express();
+  const app = express()
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(middlewares.cors);
+  app.use(helmet())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(middlewares.cors)
 
-  db.init();
+  db.init()
 
-  app.use('/auth', authorization);
-  app.use('/file', files);
+  app.use('/auth', authorization)
+  app.use('/file', files)
 
   RedisCacheClient.connect()
   app.listen(process.env.PORT, () =>
     console.log(`Express app running on port ${process.env.PORT}!`),
-  );
+  )
 } catch (error) {
-  console.log(error);
-  process.exit(1);
+  console.log(error)
+  process.exit(1)
 }
